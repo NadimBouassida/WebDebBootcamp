5f54be605e43e1061daeef7a1ffd7b9d059e2023
Nadim Bouassida <ndaemm@gmail.com>
2023-01-01 08:32:02 +0100

    removed node_modules folder

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const CampgroundSchema = new schema({
+const CampgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
     location: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+    }],
 })
 
 
